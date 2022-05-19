1637677226d522e2b21c6d5e08e2a77cb03b5fc0
Sean O'Dell <38016179+SeanO3000@users.noreply.github.com>
2022-05-18 20:54:39 -0700

    add program logic

--- ManipulateRecords/Program.cs
+++ ManipulateRecords/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ManipulateRecords.Db;
+
+// See https://aka.ms/new-console-template for more information
+
+ContactsContext Contacts = new ContactsContext();
+Contacts.Add(new Contact(
+Console.WriteLine(""
+Console.ReadLine()
+
+
+
+));
+
+Console.WriteLine("Please add a contact record.");
+
+
