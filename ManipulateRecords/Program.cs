c719110f8cf669e6dfb49f9ac9cf4e469ee53c02
Sean O'Dell <38016179+SeanO3000@users.noreply.github.com>
2022-05-23 22:28:57 -0700

    List and Delete Record

--- ManipulateRecords/Program.cs
+++ ManipulateRecords/Program.cs
@@ -1,4 +1,5 @@
 ﻿using ManipulateRecords.Db;
+using System.Linq;
 
 // See https://aka.ms/new-console-template for more information
 
@@ -15,7 +16,27 @@ using (var context = new ContactsContext())
         ContactNotes = "Learning C#"
     };
 
+    Console.WriteLine("New contact will be added. Press enter to continue...");
+    Console.ReadLine();
     context.Contacts.Add(ct);
+    context.SaveChanges();
+
+    Console.WriteLine("New contact will be listed. Press enter to continue...");
+    Console.ReadLine();
+    foreach (var v in context.Contacts)
+    {
+        Console.WriteLine(v.ContactName);
+        Console.WriteLine(v.ContactEmail);
+        Console.WriteLine(v.ContactPhoneType);
+        Console.WriteLine(v.ContactPhoneNumber);
+        Console.WriteLine(v.ContactAge);
+        Console.WriteLine(v.ContactNotes);
+    }
+    
+    Console.WriteLine("Contact will be removed. Press enter to continue...");
+    Console.ReadLine();
+    var con = context.Contacts.Where(c => c.ContactName == "Sean");
+    context.Contacts.RemoveRange();
     context.SaveChanges();
 }
 
